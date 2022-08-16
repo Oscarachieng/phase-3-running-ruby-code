4929794350fcb41d8d4692b42db8dbe1fb055480
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-16 11:12:30 +0300

    Done.

---
+++ spec/app.rb
@@ -0,0 +1,17 @@
+#without parenthesis
+puts "Hello World!"
+
+#with parenthesis
+
+#puts ("Hello Oscar, Welcome to Ruby Language For the back-end Development")
+
+#puts "Oscar Abut Achieng"
+
+print "Pass this test, please."
+
+#print "Hello This Morning Oscar Achien g Abuto, Welcome to Ruby Language."
+
+p [1,2,3]
+
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
